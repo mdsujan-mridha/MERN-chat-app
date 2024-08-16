52dfe61e8e2a8cc0472990d0b565773ac79e8655
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-08-17 01:52:18 +0600

    create search dialog

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Dialog, DialogTitle, InputAdornment, List, Stack, TextField } from "@mui/material"
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import { sampleUsers } from '../../constants/sampleData';
+import UserItem from '../shared/UserItem';
 
 const Search = () => {
+  const [users, setUsers] = useState(sampleUsers)
+  const [isSearch, setIsSearch] = useState(true);
+  const searchCloseHandler = (e) => {
+    e.preventDefault()
+    setIsSearch(prev => !prev)
+  }
+
+  const search = useInputValidation();
+
+  const addFriendHandler = (id) => { console.log(id) }
+  const isLoadingSendFriendRequest = false;
+
   return (
-    <div>Search</div>
+    <Dialog open={isSearch} onClose={searchCloseHandler}>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle> Find People </DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <List>
+          {
+            users.map((i) => (
+              <UserItem
+                user={i}
+                key={i._id}
+                handler={addFriendHandler}
+                handlerIsLoading={isLoadingSendFriendRequest}
+
+              />
+            ))
+          }
+        </List>
+
+      </Stack>
+    </Dialog>
   )
 }
 
