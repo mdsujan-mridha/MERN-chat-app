d99d2ccda20ca10dc1fc30eb7b5d318cf5926f1f
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-08-25 00:29:40 +0600

    Create notification dialog

--- client/src/components/specific/NewGroup.jsx
+++ client/src/components/specific/NewGroup.jsx
@@ -1,8 +1,16 @@
+import { Dialog, DialogTitle, Skeleton, Stack } from '@mui/material'
 import React from 'react'
 
 const NewGroup = () => {
   return (
-    <div>NewGroup</div>
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "2rem" }}>
+        <DialogTitle> New Group </DialogTitle>
+        <Stack>
+          
+        </Stack>
+      </Stack>
+    </Dialog>
   )
 }
 
--- client/src/components/specific/Notifications.jsx
+++ client/src/components/specific/Notifications.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { Avatar, Button, Dialog, DialogTitle, IconButton, ListItem, Skeleton, Stack, Typography } from '@mui/material'
+import React, { memo } from 'react'
+import { sampleNotifications } from '../../constants/sampleData';
+import { Add as AddIcon } from '@mui/icons-material';
 
 const Notifications = () => {
+
+  const isLoading = false;
+  const friendRequestHandler = ({ _id, accept }) => {
+
+  }
   return (
-    <div>Notifications</div>
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "2rem" }}>
+        <DialogTitle> Notifications </DialogTitle>
+        {
+          isLoading ? (<Skeleton />) :
+            (<>
+              {
+                sampleNotifications.length > 0 ? (
+                  sampleNotifications.map((i) =>
+                    <NotificationsItem
+                      key={i._id}
+                      sender={i.sender}
+                      handler={friendRequestHandler}
+                    />)
+                ) : (
+
+                  <Typography textAlign={"center"}> 0 Notifications</Typography>
+                )
+
+              }
+            </>)
+        }
+      </Stack>
+    </Dialog>
   )
 }
+
+const NotificationsItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+
+      >
+        <Avatar src={avatar} />
+        <Typography
+          variant="body1"
+          sx={{
+            flexGlow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%",
+          }}
+        >
+          {`${name} Send you Friend request`}
+        </Typography>
+        <Stack direction={{
+          xs: "column",
+          sm: "row"
+        }}>
+          <Button onClick={() => handler({ _id, accept: true })}>Accept</Button>
+          <Button color='error' onClick={() => handler({ _id, accept: false })}>Reject</Button>
+        </Stack>
+      </Stack>
+
+    </ListItem >
+  )
+
+})
 
 export default Notifications
